5215b2641551ba6840f127a0d0176a7e170a6d5a
Rich Hopkīns <hopmedic@hotmail.com>
2016-11-23 00:25:05 -0500

    Added Queen Attack

---
+++ csharp/queen-attack/QueenAttackKata/QueenAttackKata/Queen.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace QueenAttackKata
+{
+    public class Queen
+    {
+        public Queen(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Queens
+    {
+        private Queen black;
+        private Queen white;
+
+        public Queens(Queen white, Queen black)
+        {
+            if (white.X == black.X && white.Y == black.Y)
+                throw new ArgumentException();
+            this.white = white;
+            this.black = black;
+        }
+
+        public bool CanAttack()
+        {
+            return SameRowOrColumn() || SameDiagonal();
+        }
+
+        private bool SameDiagonal()
+        {
+            return black.X - white.X == black.Y - white.Y 
+                || black.X - white.X == white.Y - black.Y;
+        }
+
+        private bool SameRowOrColumn()
+        {
+            return black.X == white.X || black.Y == white.Y;
+        }
+    }
+}
