7e88d3caac41ef8669e3db57e620f2a80f5ed6f4
Rich Hopkīns <hopmedic@hotmail.com>
2016-11-29 11:13:50 -0500

    Added Matrix

---
+++ csharp/matrix/MatrixKata/MatrixKata/Matrix.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MatrixKata
+{
+    public class Matrix
+    {
+        private List<int>[] grid;
+
+        public Matrix(string input)
+        {
+            var rows = input.Split('\n');
+            var listOfRows = new List<List<int>>();
+            foreach (var row in rows)
+            {
+                var itemsInRow = row.Split(' ');
+                var numbers = new List<int>();
+                numbers.AddRange(itemsInRow.Select(int.Parse));
+                listOfRows.Add(numbers);
+            }
+            grid = listOfRows.ToArray();
+        }
+        
+        public int Rows => grid.Length;
+        public int Cols => grid[0].Count;
+
+        public int[] Row(int i)
+        {
+            return grid[i].ToArray();
+        }
+
+        public int[] Col(int i)
+        {
+            return grid.Select(list => list[i]).ToArray();
+        }
+    }
+}
