cfb774a8fcee1eaf47ab8b9ad71461de0ab20492
Rich Hopkīns <hopmedic@hotmail.com>
2016-12-01 21:17:17 -0500

    Added Largest Series Product

---
+++ csharp/largest-series-product/LargestSeriesKata/LargestSeriesKata/LargestSeriesProduct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace LargestSeriesKata
+{
+    public class LargestSeriesProduct
+    {
+        public static ulong GetLargestProduct(string input, int span)
+        {
+            if (InvalidSpan(input, span) || InvalidInput(input))
+                throw new ArgumentException();
+            if (input == "" || span == 0) return 1;
+            var digits = input.ToCharArray()
+                .Select(x => (int)char.GetNumericValue(x))
+                .ToArray();
+            ulong maxProduct = 0;
+            var maxStart = digits.Length - span + 1;
+
+            for (var i = 0; i < maxStart; i++)
+            {
+                ulong product = 1;
+                for (var j = i; j < i + span; j++)
+                {
+                    product *= (ulong) digits[j];
+                }
+                if (product > maxProduct + 1) maxProduct = product;
+            }
+            return maxProduct;
+        }
+
+        private static bool InvalidInput(string input)
+        {
+            return !input.All(char.IsDigit);
+        }
+
+        private static bool InvalidSpan(string input, int span)
+        {
+            return span > input.Length || span < 0;
+        }
+    }
+}
