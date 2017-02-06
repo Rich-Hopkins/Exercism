998d7987adc7dbd92f6c6878a974598020976971
Rich Hopkīns <hopmedic@hotmail.com>
2017-02-06 01:15:14 -0500

    Series Kata

---
+++ csharp/series/SeriesKata/SeriesKata/Series.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace SeriesKata
+{
+    public class Series
+    {
+        private List<int> input;
+
+        public Series(string input)
+        {
+            foreach (var number in input)
+            {
+                this.input.Add((int)char.GetNumericValue(number));
+            }
+        }
+
+        public object Slices(int size)
+        {
+            var slices = new List<int[]>();
+            for (int i = 0; i < input.Count - size; i++)
+            {
+                var slice = new List<int>();
+                for (int j = i; j < size; j++)
+                {
+
+                }
+                slices.Add(slice.ToArray());
+            }
+            return slices.ToArray();
+        }
+    }
+}
