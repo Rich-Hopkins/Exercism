2a29a4cfb062ed1cc1ae1e9ef9925314ec0754bf
Rich <hopmedic@hotmail.com>
2017-03-24 01:27:32 -0400

    Added Error-Handling

---
+++ csharp/error-handling/ErrorHandlingKata/ErrorHandlingKata/ErrorHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ErrorHandlingKata
+{
+  public class ErrorHandling
+  {
+    public static void HandleErrorByThrowingException()
+    {
+      throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string s)
+    {
+      if (s == "1") return 1;
+      else return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+      result = 1;
+      if (input == "1") return true;
+      else return false;
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableResource)
+    {
+      using (disposableResource)
+        throw new Exception();
+    }
+  }
+}
