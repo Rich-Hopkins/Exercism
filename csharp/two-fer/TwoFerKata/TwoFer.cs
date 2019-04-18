51c34aeef21cecb820275f069e69808e1942d8ff
Richard Hopkins <hopmedic@hotmail.com>
2019-04-18 16:51:10 -0400

    Updated TwoFer

--- csharp/two-fer/TwoFerKata/TwoFer.cs
+++ csharp/two-fer/TwoFerKata/TwoFer.cs
@@ -3,9 +3,9 @@
     public static class TwoFer
     {
 
-        public static string Speak(string name = null)
+        public static string Speak(string name = "you")
         {
-            return string.Format("One for {0}, one for me.", name == null ? "you" : name);
+            return string.Format("One for {0}, one for me.", name);
         }
     }
 }
