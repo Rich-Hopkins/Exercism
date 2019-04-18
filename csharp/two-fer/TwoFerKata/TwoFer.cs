217c4de84aa0a5d297370cff41bdbc90124c77fd
Richard Hopkins <hopmedic@hotmail.com>
2019-04-18 16:00:14 -0400

    Two-Fer Kata

---
+++ csharp/two-fer/TwoFerKata/TwoFer.cs
@@ -0,0 +1,11 @@
+﻿namespace TwoFerKata
+{
+    public static class TwoFer
+    {
+
+        public static string Speak(string name = null)
+        {
+            return string.Format("One for {0}, one for me.", name == null ? "you" : name);
+        }
+    }
+}
