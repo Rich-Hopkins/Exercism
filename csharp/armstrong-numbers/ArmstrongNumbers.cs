f1409a169ac8133507c89adf8681206c4607ab27
Richard Hopkins <hopmedic@hotmail.com>
2019-04-22 17:04:25 -0400

    ArmstrongNumbers

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        var numArray = number.ToString().ToCharArray();
+        double newNumber = 0;
+        foreach (var n in numArray)
+        {
+            newNumber += Math.Pow(char.GetNumericValue(n), numArray.Length);
+        }
+        return number == newNumber;
+    }
+}
