33cb7ed02d5550aef04d241584b116b607f0677e
Richard Hopkins <hopmedic@hotmail.com>
2019-04-23 15:29:32 -0400

    Darts and Collatz Conjecture Katas

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        var distance = Math.Sqrt(x * x + y * y);
+        return distance > 10 ? 0 : distance > 5 ? 1 : distance > 1 ? 5 : 10;
+    }
+}
