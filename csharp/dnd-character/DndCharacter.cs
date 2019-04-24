3e429b1020fb6ff5b9cee007ddb841ca5c71078a
Richard Hopkins <hopmedic@hotmail.com>
2019-04-24 14:37:52 -0400

    DnD Character Kata

---
+++ csharp/dnd-character/DndCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class DndCharacter
+{
+    public int Strength { get; }
+    public int Dexterity { get; }
+    public int Constitution { get; }
+    public int Intelligence { get; }
+    public int Wisdom { get; }
+    public int Charisma { get; }
+    public int Hitpoints { get; }
+
+    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0m);
+
+    public static int Ability() => new Random().Next(3, 18);
+
+    public static DndCharacter Generate() => new DndCharacter();
+
+    private DndCharacter()
+    {
+        Strength = Ability();
+        Dexterity = Ability();
+        Constitution = Ability();
+        Intelligence = Ability();
+        Wisdom = Ability();
+        Charisma = Ability();
+        Hitpoints = 10 + Modifier(Constitution);
+    }
+}
